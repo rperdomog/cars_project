f22d3a697649f2305f219b2a2a791af80b6ead91
Ricardo  Perdomo <ricardoperdomo@Ricardos-MBP.lan>
2024-04-07 22:05:15 -0400

    Completed second iteration

--- app.py
+++ app.py
@@ -5,20 +5,43 @@ import matplotlib as plt
 import numpy as np
 
 df = pd.read_csv("vehicles_us.csv")
-df = df.dropna()
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0] if isinstance(x, str) else None)
 
+# Define a function to fill missing values with the mode of each group
+def fill_missing_with_group_mode(series):
+    return series.fillna(series.mode().iloc[0])
+
+# Group by 'model' and fill missing values in 'model_year' column with group mode
+df['model_year'] = df.groupby(['model'])['model_year'].transform(fill_missing_with_group_mode)
+
+
+# Define a function to fill missing values with the median of each group
+def fill_missing_with_group_median(series):
+    return series.fillna(series.median())
+
+# Group by 'model' and 'model_year' and fill missing values in 'cylinders' column with group median
+df['cylinders'] = df.groupby(['model', 'model_year'])['cylinders'].transform(fill_missing_with_group_median)
+
+
+# Drop odometer, paint_color and is_4wd columns
+df = df.drop(['odometer', 'paint_color','is_4wd'], axis=1)
+
+# Drop missing values
+df= df.dropna()
+
 st.header("Final project sprint 4 \n")
-st.header("Cars project")
+st.header("Cars project dataset")
 st.dataframe(df)
 
 #Plot vehicles types per manufacturer
 st.header("Vehicles types by manufacturer")
 fig = px.histogram(df, x='manufacturer', color='type')
+fig.update_layout(yaxis_title='Number of Vehicles')
 st.write(fig)
 
 st.header('Conditions vs model year')
 fig2 = px.histogram(df, x='model_year', color='condition')
+fig2.update_layout(yaxis_title='Number of Vehicles')
 st.write(fig2)
 
 st.header('Compare price distribution between manufacturers')
@@ -53,7 +76,7 @@ filtered_df = df[df['manufacturer'].isin(top_brands)]
 def plot_model_popularity(df, selected_models=None):
     if selected_models is not None and 'All' not in selected_models:
         filtered_df = df[df['model'].isin(selected_models)]
-        title = f"Selected Car Models ({len(selected_models)})"
+        title = f"Car models selected, models number chosen: {len(selected_models)}"
     else:
         filtered_df = df
         title = 'All Car Models'
@@ -62,6 +85,7 @@ def plot_model_popularity(df, selected_models=None):
     fig = px.bar(x=model_counts.index, y=model_counts.values, labels={'x':'Model', 'y':'Count'},
                  title=title, color_discrete_sequence=['#007F73'])
     fig.update_layout(xaxis_tickangle=-45)
+    fig.update_layout(yaxis_title='Number of Vehicles')
     st.plotly_chart(fig)
 
 # Dropdown list with all car models
@@ -81,18 +105,17 @@ st.plotly_chart(fig)
 
 
 
-# Calculate the average price per mileage range
-mileage_bins = [0, 50000, 100000, 150000, 200000, float('inf')]
-mileage_labels = ['0-50k', '50k-100k', '100k-150k', '150k-200k', '200k+']
-df['mileage_range'] = pd.cut(df['odometer'], bins=mileage_bins, labels=mileage_labels)
-avg_price_per_range = df.groupby('mileage_range')['price'].mean().reset_index()
+# Relation between model year and price
+# Filter the DataFrame to include only price values <= 100000
+df_filtered = df[df['price'] <= 100000]
 
-# Create a scatter plot
-fig = px.scatter(avg_price_per_range, x='mileage_range', y='price', title='Average Price per Mileage Range',
-                 labels={'mileage_range': 'Mileage Range', 'price': 'Average Price'})
+# Create the scatter plot
+fig = px.scatter(df_filtered, x='model_year', y='price', title='Model Year vs Price', 
+                 labels={'model_year': 'Model Year', 'price': 'Price'},
+                 template='plotly_white', opacity=0.5, color_discrete_sequence=["#007F73"])
 
-# Update x-axis to display mileage ranges nicely
-fig.update_xaxes(tickvals=mileage_labels, ticktext=[f'{label} ({str(bin)})' for label, bin in zip(mileage_labels, mileage_bins)])
+# Update the y-axis range and tick interval
+fig.update_yaxes(range=[0, 100000], tickvals=list(range(0, 100001, 10000)))
 
-# Show the plot
+# Display the plot
 st.plotly_chart(fig)
